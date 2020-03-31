1e16015d4f15533cb5ed39fb02f8de4dbe04dc09
Martin Jarcik <2684922+yarcheek@users.noreply.github.com>
2020-03-31 20:00:30 +0200

    removing styles and defining one random avatar when server starts

--- server/index.js
+++ server/index.js
@@ -3,18 +3,16 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var avatarId = Math.floor(Math.random() * Math.floor(49)) + 1;
 
 server.listen('4444', () => {
   console.log('listening for requests...');
 });
 
-function getAvatarId() {
-  return Math.floor(Math.random() * Math.floor(49)) + 1;
-}
 
 io.on('connection', (socket) => {
   socket.on('chat', (id, username, message) => {
     console.log('message ' + id + ' received, sent by: ' + username + ', content: ' + message);
-    io.emit('chat', id, username, message, getAvatarId());
+    io.emit('chat', id, username, message, avatarId);
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { useEffect } from 'react';
 import TextField from '@material-ui/core/TextField'
-import { makeStyles } from '@material-ui/core/styles';
 import socketClient from 'socket.io-client';
 import * as uuid from 'uuid';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: '25ch',
-  },
-}));
+
 
 
 
 const client = socketClient('localhost:4444')
 
 function App() {
-  const classes = useStyles();
   const [message, setMessage] = React.useState('')
   const [userName, setUserName] = React.useState('')
   const [messageList, setMessageList] = React.useState([])
@@ -50,7 +38,7 @@ function App() {
     <div className="App">
     	<div id="controlBox">
     	  	<TextField fullWidth placeholder="Enter your username" onChange={handleUsernameChange}/>
-	      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
+	      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
 	        <TextField id="writeMessage" value={message} placeholder="Enter your message and press [ENTER]"  onChange={handleMessageChange}/>
 	      </form>
 	      <div id="toolBox">
